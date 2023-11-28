7530eed79a48de793efe80a30695785dd9a4bd45
eduardocesarmaia13 <eduardocesarmaia13@gmail.com>
2023-11-28 15:32:31 -0300

    toString

--- src/Programa/Pessoa.java
+++ src/Programa/Pessoa.java
@@ -3,36 +3,42 @@ package Programa;
 public class Pessoa {
 
     public static int counter = 1;
-
     private String name;
     private String cpf;
     private String email;
 
     public Pessoa (String name, String cpf, String email) {
-        this.name=name;
-        this.cpf=cpf;
-        this.email=email;
+        this.name = name;
+        this.cpf = cpf;
+        this.email = email;
+        counter += 1;
     }
 
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
 
-    public String cpf() {
-        return cpf;
-    }
-    public void setCpf(String cpf) {
-        this.cpf=cpf;
-    }
-
-    public String getEmail(){
+    public String getEmail() {
         return email;
     }
     public void setEmail(String email) {
-        this.email=email;
+        this.email = email;
     }
+
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String toString(String name, String cpf, String email) {
+        return "\nName: " + getName() +
+                "\nCPF: " + getCpf() +
+                "\nEmail: " + getEmail();
+    }
+
 }
