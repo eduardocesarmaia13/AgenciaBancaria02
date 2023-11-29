5eb65d56120e161865a05844e0c464e09bc6a77a
eduardocesarmaia13 <eduardocesarmaia13@gmail.com>
2023-11-28 21:11:59 -0300

    Atributos Classe-Conta

---
+++ src/Programa/Conta.java
@@ -0,0 +1,11 @@
+package Programa;
+
+public class Conta {
+
+    private static int contadorDeContas = 1;
+
+    private int numeroConta;
+    private Pessoa pessoa;
+    private double saldo = 0.0;
+
+}
